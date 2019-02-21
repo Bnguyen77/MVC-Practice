03632ccca44fc10328b022dd6d2c86fad7dc34f4
Bnguyen77 <bng7761@gmail.com>
2019-02-20 21:29:28 -0600

    first Commit

---
+++ HelloMVC/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class Customer
+    {
+        public string customerId { get; set; }
+
+        [Display(Name = "Customer Name")]
+        public string customerName { get; set; }
+
+        [Display(Name = "Contact Number")]
+        public string customerPhoneNumber { get; set; }
+    }
+}
